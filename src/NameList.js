c75a5ec09d93d1ff1995c5656a2ee187f104e054
Hugo Gresse <hugo.gresse@teads.tv>
2018-04-29 15:37:38 +0200

    Fix empty localStorage

--- src/NameList.js
+++ src/NameList.js
@@ -25,7 +25,7 @@ class NameList extends Component {
   constructor(props) {
     super(props);
 
-    const selected = JSON.parse(localStorage.selectedName)
+    let selected = JSON.parse(localStorage.selectedName)
     if(!selected){
       selected = []
     }
